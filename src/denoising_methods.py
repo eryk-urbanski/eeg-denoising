4f7a005fc7f8998dd686c64f2dd7f26ba13504ea
eryk-urbanski <eryk.urbanski14@gmail.com>
2025-04-21 15:50:41 +0200

    bulk add progress 21.04

--- src/denoising_methods.py
+++ src/denoising_methods.py
@@ -3,24 +3,23 @@
 import pywt
 import numpy as np
 
-def denoise_wt(eeg_signal, wavelet='haar', level=3):
-    
-    coeffs = pywt.wavedec(eeg_signal, wavelet, mode="per")
-    coeffs[1:] = [pywt.threshold(c, np.std(c), mode='hard') for c in coeffs[1:]]
-    denoised_eeg = pywt.waverec(coeffs, wavelet)
-    
-    return denoised_eeg
-
 def maddest(d, axis=None):
     return np.mean(np.absolute(d - np.mean(d, axis)), axis)
 
-def denoise(x, wavelet='haar', level=1):
+def denoise(x, wavelet='haar', level=1, threshold_mode='hard'):
     coeffs = pywt.wavedec(x, wavelet, mode="per")
     sigma = (1/0.6745) * maddest(coeffs[-level])
 
     uthresh = sigma * np.sqrt(2*np.log(len(x)))
-    coeffs[1:] = (pywt.threshold(i, value=uthresh, mode='hard') for i in coeffs[1:])
+    coeffs[1:] = (pywt.threshold(i, value=uthresh, mode=threshold_mode) for i in coeffs[1:])
 
     ret = pywt.waverec(coeffs, wavelet, mode='per')
     
     return ret
+
+def denoise_multichannel(X, wavelet='haar', level=1, threshold_mode='hard'):
+    denoised = np.array([
+        denoise(row, wavelet=wavelet, level=level, threshold_mode=threshold_mode)
+        for row in X
+    ])
+    return denoised
